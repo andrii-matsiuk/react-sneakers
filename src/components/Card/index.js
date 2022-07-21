36053acda202f696babaf80a01c8b58f8371ca1a
andrii-matsiuk <amatsiuk@MacBook-Air-Andrij.local>
2022-07-21 11:28:32 +0300

    css modules

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,13 @@
+import styles from './Card.module.scss';
+
 function Card(props) {
     const onClickButton = () => {
         alert(props.title)    
     }
 
     return (
-        <div className="card">
-            <div className="favorite">
+        <div className={styles.card}>
+            <div className={styles.favorite}>
                 <img src="/img/heart-unliked.svg" alt="unliked" />
             </div>
             <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
