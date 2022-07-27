c69bcaf9c582083a19410c8882fe911597350e87
andrii-matsiuk <amatsiuk@MacBook-Air-Andrij.local>
2022-07-27 23:48:59 +0300

    empty cart view and button back

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
@@ -11,25 +12,43 @@ function App() {
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch('https://62d99d305d893b27b2ea75e5.mockapi.io/items')
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      setItems(json);
+    // // отримання даних методом fetch
+    // fetch('https://62d99d305d893b27b2ea75e5.mockapi.io/items')
+    // .then((res) => {
+    //   return res.json();
+    // })
+    // .then((json) => {
+    //   setItems(json);
+    // });
+
+    // отримання товарів із сервера через бібліотеку axios
+    axios.get('https://62d99d305d893b27b2ea75e5.mockapi.io/items').then(res => {
+      setItems(res.data);
+    });
+    //отримання товарів доданих в корзину на сервері
+    axios.get('https://62d99d305d893b27b2ea75e5.mockapi.io/cart').then(res => {
+      setCartItems(res.data);
     });
   }, []);
 
   const onAddToCart = (obj) => {
     // пошук елемента в масиві вже доданих в корзину
     let itemFound = cartItems.find(item => item.title === obj.title);
-    //console.log(cartItems.find(item => item.title === obj.title));
+    
     if (itemFound===undefined) {
+      //додаємо вибраний товар на бекенд в корзину з допомогою бібліотеки axios
+      axios.post('https://62d99d305d893b27b2ea75e5.mockapi.io/cart', obj);
+      /////
       setCartItems(prev => [... prev, obj]); // аналогія в реакт метода пуш для масиву, коли хочемо додати обєкт в нього    
     }
     else {
       setCartItems(cartItems.filter(p => p.title !== itemFound.title));
     }
+  }
+
+  const onRemoveItem = (id) => {
+    axios.delete(`https://62d99d305d893b27b2ea75e5.mockapi.io/cart/${id}`);
+    setCartItems(prev => prev.filter(item => item.id !== id)); 
   }
 
   const onChangeSearchInput = (event) => {
@@ -39,7 +58,7 @@ function App() {
 
   return (
     <div className="wrapper clear">
-      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} /> }
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove = {onRemoveItem} /> }
        
       <Header onClickCart={() => setCartOpened(true)} />
 
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,36 +1,52 @@
-function Drawer({onClose, items=[]}) {
+function Drawer({ onClose, onRemove, items = [] }) {
     return (
         <div className="overlay">
             <div className="drawer">
                 <h2 className="mb-30 d-flex justify-between">Cart<img onClick={onClose} className="removeBtn cu-p" src="/img/btn-remove.svg" alt="remove" /></h2>
 
-                <div className="items">
-                    {items.map((obj) => (
-                        <div className="cartItem d-flex align-center mb-20">
-                            <img className="mr-20" width={70} height={70} src={obj.imageUrl} alt="" />
-                            <div className="mr-20">
-                                <p className="mb-5">{obj.title}</p>
-                                <b>{obj.price} USD</b>
-                            </div>
-                            <img className="removeBtn" src="/img/btn-remove.svg" alt="remove" />
+                {items.length > 0 ?
+                    (<div>
+                        <div className="items">
+                            {items.map((obj) => (
+                                <div className="cartItem d-flex align-center mb-20">
+                                    <img className="mr-20" width={70} height={70} src={obj.imageUrl} alt="" />
+                                    <div className="mr-20">
+                                        <p className="mb-5">{obj.title}</p>
+                                        <b>{obj.price} USD</b>
+                                    </div>
+                                    <img className="removeBtn" onClick={() => onRemove(obj.id)} src="/img/btn-remove.svg" alt="remove" />
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-                <div className="cartTotalBlock">
-                    <ul>
-                        <li>
-                            <span>Summary:</span>
-                            <div></div>
-                            <b>1205 USD</b>
-                        </li>
-                        <li>
-                            <span>Tax 5%:</span>
-                            <div></div>
-                            <b>205 USD</b>
-                        </li>
-                    </ul>
-                    <button className="greenButton">Order<img src="/img/arrow.svg" alt="arrow" /></button>
-                </div>
+                        <div className="cartTotalBlock">
+                            <ul>
+                                <li>
+                                    <span>Summary:</span>
+                                    <div></div>
+                                    <b>1205 USD</b>
+                                </li>
+                                <li>
+                                    <span>Tax 5%:</span>
+                                    <div></div>
+                                    <b>205 USD</b>
+                                </li>
+                            </ul>
+                            <button className="greenButton">Order<img src="/img/arrow.svg" alt="arrow" /></button>
+                        </div>
+                    </div>)
+                :
+                    (<div className="cartEmpty d-flex align-center justify-center flex-column flex">
+                        <img className="mb-20" width="120px" height="120px" src="/img/empty-cart.jpg" alt="empty-cart" />
+                        <h2>Cart is empty</h2>
+                        <p className="opacity-6">Add at least one pair of sneackers to make the order</p>
+                        <button className="greenButton" onClick={onClose}>
+                            <img src="/img/arrow.svg" alt="arrow" />
+                            Back to shop
+                        </button>
+                    </div>
+                    )
+                }
+
             </div>
         </div>
     )
