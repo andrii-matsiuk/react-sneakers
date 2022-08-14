729deafa38624ca584e6130af370200524ae0381
andrii-matsiuk <amatsiuk@MacBook-Air-Andrij.local>
2022-08-14 23:49:13 +0300

    fix with order and fav pages

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ function App() {
 
         <Header onClickCart={() => setCartOpened(true)} />
 
-        <Route path="" exact>
+        <Route path="/" exact>
           <Home
             items={items}
             cartItems={cartItems}
@@ -141,11 +141,11 @@ function App() {
           />
         </Route>
 
-        <Route path="favorites" exact>
+        <Route path="/favorites" exact>
           <Favorites />
         </Route>
 
-        <Route path="orders" exact>
+        <Route path="/orders" exact>
           <Orders />
         </Route>
 
